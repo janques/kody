61dfbc2de4e445039f122b97ce71e76b5d1f2f00
janques <jankielcz00@wp.pl>
2017-09-26 21:04:58 +0200

    największa ABC dokończenie lekcji

--- cpp/najwiekszaABC.cpp
+++ cpp/najwiekszaABC.cpp
@@ -19,17 +19,27 @@ int main(int argc, char **argv)
     {
         if (a > c)
             cout << "Największe a=" << a;
-        else
+        else if ( a < c )
             cout << "Największe c=" << c;
+        else
+            cout << "Największymi liczbami są a="<<a<<" i c="<<c<< endl;
     } 
     else if (b > a)
     {
         if (b > c)
             cout << "Największe b=" << b;
-        else
+        else if ( b < c)
             cout << "Największe c=" <<c;
+        else
+            cout << "Największymi liczbami są b="<<b<<" i c="<<c<< endl;
     } 
-    
+    else if ( a==b ) 
+    {
+        if ( a==c )
+            cout << "Wszystkie liczby są równe" <<endl;
+        else if ( a > c )
+            cout << "Największymi liczbami są a="<<a<<" i b="<<b<< endl;
+    }
 	return 0;
 }
 
