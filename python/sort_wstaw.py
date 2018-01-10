6714c7d1af5588043cd6b79ea5ff29ddf2c93a36
janques <jankielcz00@wp.pl>
2018-01-10 12:36:26 +0100

    lekcja 10.01.18

--- python/sort_wstaw.py
+++ python/sort_wstaw.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from math import floor
+
 
 def sort_wstaw(lista):
     """wersja liniowa"""
@@ -13,8 +15,10 @@ def sort_wstaw(lista):
         lista[k + 1] = el  # wstawianie elementu
     return lista
 
-def wyszukaj_bin_it(lista, el):
-    lewy, prawy = 0, len(lista) - 1
+def wyszukaj_bin(lewy, prawy, lista, el):
+    """Wersja iteracyjna wyszukiwania binarnego,
+    wyszukujemy indeks do wstawienia elementu"""
+    # lewy, prawy = 0, len(lista) - 1
     while lewy < prawy:
         srodek = floor((lewy + prawy) / 2)
         # print(srodek)
@@ -22,21 +26,21 @@ def wyszukaj_bin_it(lista, el):
             prawy = srodek
         else:
             lewy = srodek + 1
-    if lista[lewy] == el:
-        return lewy  # znaleziono element
 
+    return lewy # nie znaleziono elementu
 
-    return -1  # elementu nie znaleziono
 
 
 def sort_wstaw_bin(lista):
     """wersja binarna"""
     for i in range(1, len(lista)):
         el = lista[i]
-        k = wyszukaj_bin(lewy, prawy, lista, el)
+        k = wyszukaj_bin(0, i, lista, el)
 
         # tworzenie listy z wstawionym elementem
-        lista =
+        lista = lista[:k] + [el] + lista[k:i] + lista[i + 1:]
+        print(lista)
+    return lista
 
 def main(args):
     lista = [4, 3, 7, 0, 2, 3, 1, 9,]
@@ -45,7 +49,8 @@ def main(args):
     # [3, 4, 7, 0, 2, 3, 1, 9]
     # [3, 4, 7, 0, 2, 3, 1, 9]
     # [0, 3, 4, 7, 2, 3, 1, 9]
-    print(sort_wstaw(lista))
+    # print(sort_wstaw(lista))
+    sort_wstaw_bin(lista)
     return 0
 
 
