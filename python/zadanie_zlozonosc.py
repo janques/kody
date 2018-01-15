7faf4ce862b084bda4c13b655828c9e061f03b6c
janques <jankielcz00@wp.pl>
2018-01-15 17:14:10 +0100

    zadanie_złożoność/pd

---
+++ python/zadanie_zlozonosc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  zadanie_zlozonosc.py
+#  
+#  
+
+
+def wypisz_nieparzyste(n):
+    for i in range(1, n, 2):
+        print(i)
+
+
+
+def main(args):
+
+    n = int(input("Podaj wartosc liczby n: "))
+
+    wypisz_nieparzyste(n)
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
