cee072455f672e0306fb38a35330e2ef9285e15a
janques <jankielcz00@wp.pl>
2018-01-16 13:31:49 +0100

    teoria o algorytmach

--- cpp/zadanie_zlozonosc.cpp
+++ cpp/zadanie_zlozonosc.cpp
@@ -9,14 +9,6 @@
 
 using namespace std;
 
-void wypisz_nieparzyste(int n)
-{
-    for(int i=1;i<=n;i+=2)
-    {
-        cout<<i<<endl;
-    }
-}
-
 
 int main(int argc, char **argv)
 {
@@ -24,7 +16,10 @@ int main(int argc, char **argv)
     cout<<"Podaj wartosc liczby n: ";
     cin>>n;
     
-    wypisz_nieparzyste(n);
+    for(int i=1;i<n;i+=2)
+    {
+        cout<<i<<endl;
+    }
 
 	return 0;
 }
