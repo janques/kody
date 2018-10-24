2ecc73966d1fe6394e7e26d5daf86c3919d22ee7
janques <jankielcz00@wp.pl>
2018-10-24 09:40:39 +0200

    ciagle baza

--- baza/dane_uczniow/kwerendy.py
+++ baza/dane_uczniow/kwerendy.py
@@ -9,8 +9,17 @@ import sqlite3
 def kwerenda1(cur):
     
     cur.execute("""
-        SELECT * FROM nazwiska WHERE nazwisko LIKE 'G%'
+        SELECT * FROM nazwiska
+        INNER JOIN oceny ON nazwiska.nr_ucznia = oceny.nr_ucznia
     """)
+    
+    
+    # WHERE miejsce_urodz <> 'Gdańsku'
+    # WHERE miejsce_urodz = 'Gdańsku'
+    # SELECT * FROM nazwiska INNER JOIN dane_osobowe ON nazwiska.nr_ucznia = dane_osobowe.nr_ucznia
+    # SELECT COUNT (*) FROM nazwiska WHERE imie1 LIKE 'A__a'
+    # SELECT * FROM nazwiska WHERE imie1 LIKE 'A__a'
+    # SELECT * FROM nazwiska WHERE nazwisko LIKE 'G%'
     for row in cur.fetchall():
         print(tuple(row))
 
