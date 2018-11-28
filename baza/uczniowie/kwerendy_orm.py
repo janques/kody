986940d19fcbd94bd6900b96899f4a0977a57c0e
janques <jankielcz00@wp.pl>
2018-11-28 09:40:38 +0100

    kwerendy

--- baza/uczniowie/kwerendy_orm.py
+++ baza/uczniowie/kwerendy_orm.py
@@ -6,6 +6,46 @@ import os
 from uczniowie_model import *
 
 
+
+def kw08():
+    """lista nazwisk uczniów i nazw klas"""
+    query = (Uczen 
+        .select(Uczen.nazwisko, Uczen.klasa.klasa)
+        .join(Klasa)
+    )
+    for obj in query:
+        print(obj.nazwisko, obj.klasa.klasa)
+
+
+
+def kw09():
+    """lista klas i ilośc uczniów"""
+    query = (Uczen 
+        .select(fn.Count(Uczen.id).alias('ilu'), Uczen.klasa.klasa)
+        .join(Klasa)
+        .group_by(Klasa)
+        .order_by(SQL('ilu').desc())
+    )
+    for obj in query:
+        print(obj.klasa.klasa, obj.ilu)
+        
+        
+def kw10():
+    """lista przedmiotów i ilośc ocen"""
+    query = (Ocena 
+        .select(fn.Count(Ocena.ocena).alias('ilu'), Ocena.przedmiot.przedmiot)
+        .join(Przedmiot)
+        .group_by(Przedmiot)
+        .order_by(SQL('ilu').desc())
+    )
+    for obj in query:
+        print(obj.przedmiot.przedmiot, obj.ilu)
+        
+
+def kw11():
+    """suma wszystkich ocen"""
+
+        
 def main(args):
     baza.connect() 
 
@@ -19,8 +59,10 @@ def main(args):
         "Uczen.select().where(Uczen.plec==1).order_by(Uczen.egzmat.asc())",
     ]
     
-    for obj in eval(kwerendy[5]):
-        print(obj.nazwisko, obj.imie, obj.egzmat)
+   # for obj in eval(kwerendy[5]):
+   #     print(obj.nazwisko, obj.imie, obj.egzmat)
+    
+    kw10()
     
     baza.commit()
     baza.close()
