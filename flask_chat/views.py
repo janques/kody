1da2c63c2ac301057ad67b7f8ec96f03749bef25
janques <jankielcz00@wp.pl>
2019-01-30 09:40:46 +0100

    DLA WICIA :DD

--- flask_chat/views.py
+++ flask_chat/views.py
@@ -90,8 +90,37 @@ def usun(pid):
         return redirect(url_for('index'))
     return render_template('pytanie_usun.html', pytanie=p)
     
+@app.route('/edytuj/<int:pid>', methods=['GET', 'POST'])
+def edytuj(pid):
+    """Edycja pytań i odpowiedzi"""
+    p = get_or_404(pid)
+    form = DodajForm(obj=p)
+    form.kategoria.choices = [(k.id, k.kategoria) for k in Kategoria.select()]
+    form.kategoria.data = p.kategoria.id
     
+    if form.validate_on_submit():
+        p.pytanie = form.pytanie.data
+        p.kategoria = form.kategoria.data
+        p.save()
+        for o in form.odpowiedzi.data:
+            odp = Odpowiedz.get_by_id(o['id'])
+            odp.odpowiedz=o['odpowiedz']
+            odp.odpok=int(o['odpok'])
+            odp.save()
+        flash("Zaktualizowano pytanie: {}".format(form.pytanie.data))
+        redirect(url_for('lista'))
+    else:
+        flash_errors(form)
+        
     
+    odpowiedzi = []
+    for o in Odpowiedz.select().where(Odpowiedz.pytanie == p.id).dicts():
+        odpowiedzi.append(o)
+    form.odpowiedzi(data=odpowiedzi)
+    
+    return render_template('edytuj.html', form=form)
+        
+
     
     
     
