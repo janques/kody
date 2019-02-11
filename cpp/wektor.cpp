41ca7cb385a382c864f9468d56cc87611a15b517
janques <jankielcz00@wp.pl>
2019-02-11 14:20:22 +0100

    nie wiem

---
+++ cpp/wektor.cpp
@@ -0,0 +1,32 @@
+/*
+ * wektor.cpp
+ * 
+ * 
+ */
+
+//Program:
+//1) pobiera od użytkownika wspolrzedne punktu poczatkowego i koncowego
+//2) oblicz wspolrzedne srodka tego wektora
+
+//Uzyj dwoch struktur:
+//punkt - do przechowywania wspolrzednych punktu
+//wektor - do przechowywania wspolrzednych punktu poczatkowego i koncowego wektora
+
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct wektor()
+{
+    int pp=0;
+    int pk=0;
+};
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
