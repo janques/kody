fa0c49b38c3b74f0c0d5cd525e892cd66ee0efd3
janques <jankielcz00@wp.pl>
2019-02-18 12:50:25 +0100

    dla gnacika i tamyla :D

--- cpp/wektor.cpp
+++ cpp/wektor.cpp
@@ -18,15 +18,58 @@
 
 using namespace std;
 
-struct wektor()
+struct punkt
 {
-    int pp=0;
-    int pk=0;
+    int x;
+    int y;
 };
+
+struct wektor
+{
+    punkt pp;
+    punkt pk;
+    
+};
+
+punkt wylicz_srodek(wektor w) {
+    punkt ps;
+    ps.x = (w.pp.x + w.pk.x)/2;
+    ps.y = (w.pp.y + w.pk.y)/2;
+    
+    
+    
+    return ps;
+    
+}
+
+
+
+wektor getWektor() {
+    wektor w1;
+    cout << "Podaj x, y punktu poczatkowego: ";
+    cin >> w1.pp.x;
+    cin >> w1.pp.y;
+    cout << "Podaj x, y punktu końcowego: ";
+    cin >> w1.pk.x;
+    cin >> w1.pk.y;
+
+    
+    return w1;
+    
+}
+
+
 
 int main(int argc, char **argv)
 {
-	
-	return 0;
+	wektor w;
+    w = getWektor();
+    cout << "(" << w.pp.x << "," << w.pp.y << ")" << endl;
+    cout << "(" << w.pk.x << "," << w.pk.y << ")" << endl;
+    punkt ps;
+    ps = wylicz_srodek(w);
+    cout << "(" << ps.x << "," << ps.y << ")" << endl;
+    
+    return 0;
 }
 
